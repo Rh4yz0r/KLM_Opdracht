39f8ecbee99037ac983aa6b5960099988b0c2ccd
Chris Desktop <chrishuider@gmail.com>
2022-01-21 19:03:14 +0100

    Added summaries and regions to GameManager script.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,32 +5,34 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [Header("Prefabs")]                             //Prefab GameObjects
+    #region Variables
+    [Header("Prefabs")]                                             //Prefab GameObjects
     public GameObject hangarPrefab;
     public GameObject planePrefab;
 
-    [Header("Scene Components")]                    //Scene Component GameObjects
+    [Header("Scene Components")]                                    //Scene Component GameObjects
     public GameObject directionalLight;
 
-    [Header("User Interface Components")]           //User Interface GameObjects
+    [Header("User Interface Components")]                           //User Interface GameObjects
     public GameObject startButton;
     public GameObject startScreen;
     public GameObject planeAmountText;
     public GameObject parkedText;
 
-    [Header("Parent Components")]                   //Instantiated GameObject Parents
+    [Header("Parent Components")]                                   //Instantiated GameObject Parents
     public Transform hangarParent;
     public Transform planeParent;
 
-    private int planeAmount = 3;
+    [Header("Airplane Scriptable Objects")]                         //List with Airplane ScriptableObjects
+    public List<Airplane> planeTypes = new List<Airplane>();
 
     private List<GameObject> planes = new List<GameObject>();
     private List<GameObject> hangars = new List<GameObject>();
 
-    [Header("Airplane Scriptable Objects")]
-    public List<Airplane> planeTypes = new List<Airplane>();
+    private int planeAmount = 3;
 
     private bool gameStarted;
+    #endregion
 
     void Start()
     {
@@ -42,6 +44,10 @@ public class GameManager : MonoBehaviour
         if (gameStarted && CheckPlanesParked()) parkedText.SetActive(true);
     }
 
+    /// <summary>
+    /// Add numbers to every GameObject with given tag.
+    /// </summary>
+    /// <param name="tag"></param>
     private void SetNumberForTags(string tag)
     {
         int number = 1;
@@ -54,13 +60,19 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Set amount of planes to be instantiated.
+    /// </summary>
+    /// <param name="amount"></param>
     public void SetPlaneAmount(float amount)
     {
         planeAmount = (int)amount;
         planeAmountText.GetComponent<TextMeshProUGUI>().text = planeAmount.ToString();
     }
 
-
+    /// <summary>
+    /// Instantiate and set tags for planes and hangars, remove start screen menu and start the game.
+    /// </summary>
     public void StartGame()
     {
         Vector3 hangarPosition = new Vector3(-8.75f, 0, 4);
@@ -84,6 +96,10 @@ public class GameManager : MonoBehaviour
         gameStarted = true;
     }
 
+    /// <summary>
+    /// Check if all the planes have parked.
+    /// </summary>
+    /// <returns></returns>
     private bool CheckPlanesParked()
     {
         foreach (GameObject plane in planes)
@@ -93,6 +109,9 @@ public class GameManager : MonoBehaviour
         return true;
     }
 
+    /// <summary>
+    /// Start parking Coroutine for every plane.
+    /// </summary>
     public void ParkPlanes()
     {
         foreach (GameObject plane in planes)
@@ -107,6 +126,12 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Initiate parking for given plane to given hangar.
+    /// </summary>
+    /// <param name="plane"></param>
+    /// <param name="hangar"></param>
+    /// <returns></returns>
     private IEnumerator StartParking(GameObject plane, GameObject hangar)
     {
         Vector3 StartParkingPosition = hangar.transform.position - new Vector3(0, 0, 2);
@@ -119,6 +144,9 @@ public class GameManager : MonoBehaviour
         planeFunc.parked = true;
     }
 
+    /// <summary>
+    /// Switch lights on and off for every plane.
+    /// </summary>
     public void SwitchLights()
     {
         directionalLight.SetActive(!directionalLight.activeInHierarchy);
